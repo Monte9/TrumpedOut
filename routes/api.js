0150b7cdb2e7e142e81c2285c316c1c24e58e659
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2016-02-27 14:35:53 -0800

    Response sent

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,25 @@ var router = express.Router();
 /* Main entry point for prompt requests */
 router.get('/api/1.0', function(req, res, next) {
 	//Output the basic request sent from parse
-	console.log(req.body);
-	
-  res.json(req.body);
+
+  	var response = {
+	   sendmms: true,
+	   showauthurl: false,
+	   authstate: null,
+	   text: "Hello World! You said 'This is a test call!'.",
+	   speech: "Hello World! You said 'This is a test call!'.",
+	   status: "OK",
+	   webhookreply: null,
+	   images: [
+	      {
+	         imageurl: "http://api.dev.promptapp.io/images/random/helloworld.gif",
+	         alttext: "Hello World!"
+	      }
+	   ]
+	}
+
+	console.log(response);
+  	res.send(response);
 });
 
 module.exports = router;
