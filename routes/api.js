75028fefea68014d8e14581e5a82b6a4a998c96c
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2016-02-27 16:36:50 -0800

    Changed get to post

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* Main entry point for prompt requests */
-router.get('/api/1.0', function(req, res, next) {
+router.post('/api/1.0', function(req, res, next) {
 	//Output the basic request sent from parse
 
   	var response = {
